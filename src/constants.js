27b39598e06a7d25616d59d80dd0e695bf4254c6
Mason Michalec <mason_michalec@epam.com>
2022-05-17 15:35:24 +0200

    removed unnecessary extra path for non-existant task

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,9 @@
 const csvFilePath_task2 = './csv/nodejs-hw1-ex1.csv';
 const outputDirectory = './output/';
 const outputFilePath_task2 = './output/csvToText_task2.txt';
-const outputFilePath_task3 = './output/csvToText_task3.txt';
 
 module.exports = {
   csvFilePath_task2,
   outputDirectory,
-  outputFilePath_task2,
-  outputFilePath_task3
+  outputFilePath_task2
 };
--- src/task2.js
+++ src/task2.js
@@ -1,12 +1,6 @@
 import csv from 'csvtojson';
 import fs from 'fs';
-import {
-  csvFilePath_task2 as csv1,
-  csvFilePath_task3 as csv2,
-  outputDirectory as od,
-  outputFilePath_task2 as ofp1
-} from './constants';
-import { structureData } from './helpers';
+import { csvFilePath_task2 as csv1, outputDirectory as od, outputFilePath_task2 as ofp1 } from './constants';
 
 async function main() {
   if (!fs.existsSync(od)) {
