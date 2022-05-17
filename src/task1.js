231495e879a0777c757466e8f82a6f18c0948ff5
Mason Michalec <mason_michalec@epam.com>
2022-05-17 13:50:34 +0200

    Added task1.1, babel, gitignore

---
+++ src/task1.js
@@ -0,0 +1,22 @@
+import * as readline from 'readline';
+import { stdin as input, stdout as output } from 'process';
+
+const rl = readline.createInterface({ input, output, terminal: true });
+const CODES = {
+  EXIT: 'exit'
+};
+
+function getReversedString(str) {
+  return str.split('').reverse().join('');
+}
+
+function main() {
+  rl.question('Enter a string to reverse: ', (text) => {
+    if (text === 'exit') return rl.close();
+    else rl.write(getReversedString(text) + '\n');
+  });
+}
+
+rl.on('line', main);
+
+main();
